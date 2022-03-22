51b94048946d6565f361d7f66f178b05f393fb0a
nathanzd <nathan.fe.dias@gmail.com>
2022-03-22 14:36:25 -0300

    Final

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,8 +3,26 @@ import styles from './Home.module.css'
 
 
 const Home = ({row,text,text2,foto,title,link}) => {
+    
   return (
+  <div>
     <a href={link}><div id="sobre" style={{flexDirection:row ? 'row':'row-reverse'}} className={`${styles.home_container}`}>
+      
+        <div className={`${styles.perfil_foto}`}>
+          <img src={foto}/>
+        </div>
+        <div className={`${styles.sobre} ${styles.animeLeft}  `}>
+            <h1 >{title}</h1>
+            <p>{text}
+              <br/><br/>
+              {text2}
+            </p>
+        </div>
+     
+    </div></a>
+
+    <a href={link}><div className={`${styles.container}`}>
+      
       <div className={`${styles.perfil_foto}`}>
         <img src={foto}/>
       </div>
@@ -15,7 +33,9 @@ const Home = ({row,text,text2,foto,title,link}) => {
             {text2}
           </p>
       </div>
-    </div></a>
+   
+  </div></a>
+    </div>
   )
 }
 
